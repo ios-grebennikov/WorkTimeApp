42767e9737afb051865a65a46cbdaa615f090c4e
Alexey Grebennikov <ios.grebennikov@gmail.com>
2023-03-23 16:18:29 +0300

    ADD: - Initial commit

---
+++ WorkTimeApp/WorkTimeApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WorkTimeApp
+//
+//  Created by Alexey Grebennikov on 23.03.23.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
